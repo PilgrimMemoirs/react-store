db2e73ffd77c6ed552f2616fb91ff6306e71a7c3
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2020-09-21 12:25:34 -0700

    add homepage component

---
+++ src/homepage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">HATS</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+        <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+        <div className="menu-item">
+        <div className="content">
+            <h1 className="title"></h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+    </div>
+    </div>
+)
+
+export default HomePage;
